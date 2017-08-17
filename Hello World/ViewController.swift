9d8462130c0e7a71dc5dc1871415e1d542c64d0b
MINAGI <minagi@MINAGI-no-MacBook-Pro.local>
2017-08-18 02:27:13 +0900

    first commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        outputLabel.text = "Hello, World"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var outputLabel: UILabel!
 
 }
 
